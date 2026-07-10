8b954d8d284309432775e7c009e7210aa60a39de
Huulinh1312 <nguyenhuulinh52561@gmail.com>
2026-07-10 12:43:43 +0700

    LayDanhSachPhanTRang

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -5,11 +5,35 @@ export const movieApi = {
     getMovieList: (maNhom = 'GP01') => {
         return axiosInstance.get(`/QuanLyPhim/LayDanhSachPhim?maNhom=${maNhom}`)
     },
+    // Lấy danh sách phim phân trang (cho Admin)
+    getMovieListPhanTrang: (maNhom = 'GP01', soTrang = 1, soPhanTuTrenTrang = 10) => {
+        return axiosInstance.get(`/QuanLyPhim/LayDanhSachPhimPhanTrang?MaNhom=${maNhom}&soTrang=${soTrang}&soPhanTuTrenTrang=${soPhanTuTrenTrang}`)
+    },
     // Lấy chi tiết phim
     getMovieDetail: (maPhim) => {
         return axiosInstance.get(`/QuanLyPhim/LayThongTinPhim?maPhim=${maPhim}`)
     },
-     // lấy danh sách banner
+    // Thêm phim mới (upload hình)
+    addMovieWithImage: (movieData) => {
+        return axiosInstance.post('/QuanLyPhim/ThemPhimUploadHinh', movieData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+    },
+    // Cập nhật phim (upload hình)
+    updateMovieWithImage: (movieData) => {
+        return axiosInstance.post('/QuanLyPhim/CapNhatPhimUpload', movieData, {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            }
+        })
+    },
+    // Xóa phim
+    deleteMovie: (maPhim) => {
+        return axiosInstance.delete(`/QuanLyPhim/XoaPhim?MaPhim=${maPhim}`)
+    },
+    // lấy danh sách banner
     getBanners: () => {
         return axiosInstance.get('/QuanLyPhim/LayDanhSachBanner')
     }
